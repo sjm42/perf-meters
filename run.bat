adc78bc29181c0627c0deafe25c95dad4a10db52
Sami J. Mäkinen <sjm@iki.fi>
2022-12-08 14:29:22 +0200

    Testing...

--- run.bat
+++ run.bat
@@ -4,11 +4,11 @@ perf_meters ^
 --cpu-pwm-min 0 ^
 --cpu-pwm-max 226 ^
 --net-gauge-mbps 40 ^
---net-pwm-min 4 ^
---net-pwm-zero 108 ^
---net-pwm-max 212 ^
+--net-pwm-min 12 ^
+--net-pwm-zero 113 ^
+--net-pwm-max 220 ^
 --mem-pwm-min 0 ^
---mem-pwm-max 226 ^
+--mem-pwm-max 228 ^
 --pwm-max-delta 16 ^
 --samplerate 4 ^
--v --port COM7
+-v --port COM8
