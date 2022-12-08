a6d5be6863e14999c65698c12737af43a532aee6
Sami J. Mäkinen <sjm@iki.fi>
2022-12-08 13:56:31 +0200

    Added run.bat for testing in windoze

---
+++ run.bat
@@ -0,0 +1,14 @@
+rem For K. Laakso by yours truly
+
+perf_meters ^
+--cpu-pwm-min 0 ^
+--cpu-pwm-max 226 ^
+--net-gauge-mbps 40 ^
+--net-pwm-min 4 ^
+--net-pwm-zero 108 ^
+--net-pwm-max 212 ^
+--mem-pwm-min 0 ^
+--mem-pwm-max 226 ^
+--pwm-max-delta 16 ^
+--samplerate 4 ^
+-v --port COM7
